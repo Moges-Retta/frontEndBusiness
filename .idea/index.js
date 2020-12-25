f8d4c9979642729d197a444a4a11033649b1dd34
Moges <Moges.ash@gmail.com>
2020-12-25 19:27:02 +0100

    Final entry for DB done.

--- .idea/index.js
+++ .idea/index.js
@@ -5,7 +5,7 @@ const searchUrl = "http://localhost:8080/search?keyword=";
 const textSearchButton = document.getElementById("mainSearch");
 const productUrl = "http://localhost:8080/products/";
 
-var idRandom = Math.floor((Math.random() * 12) + 1);
+var idRandom = Math.floor((Math.random() * 16) + 1);
 var urlProducts = catagoryUrl.concat("/").concat(idRandom.toString());
 
 var catagoryList = sessionStorage.getItem("catagories");// avoid request to database
